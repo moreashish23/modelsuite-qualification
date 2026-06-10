1a510f37c99ba9842ed9ed89141113e7aaa99834
moreashish23 <moreashishshivaji23@gmail.com>
2026-06-10 17:14:28 +0530

    fix: resolve sidebar layout and lint issues

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,22 +1,33 @@
-const js = require('@eslint/js')
-const { defineConfig } = require('eslint/config')
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import reactPlugin from 'eslint-plugin-react'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-module.exports = defineConfig([
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    files: ['**/*.js'],
-    extends: [js.configs.recommended],
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    plugins: {
+      react: reactPlugin,
+    },
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'commonjs',
-      globals: {
-        Buffer: 'readonly',
-        __dirname: 'readonly',
-        __filename: 'readonly',
-        console: 'readonly',
-        module: 'readonly',
-        process: 'readonly',
-        require: 'readonly',
-      },
+      globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    rules: {
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true, allowExportNames: ['useAuth'] },
+      ],
     },
   },
 ])
